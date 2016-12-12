f0964106646c7c7d69fffc17f6a087867dba1f14
Peter Gleixner <pg@scherer-software.de>
2016-12-12 09:34:47 +0100

    done

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 var data = [
 	{
 		"id": 1,
-		"author": "me",
+		"author": "gleixnerp",
 		"message": "this is a message",
 		"date": "07/12/2016"
 	},
 	{
 		"id": 2,
-		"author": "me",
+		"author": "gleixnerp",
 		"message": "this is another message, even longer text and tomorrow",
 		"date": "08/12/2016"
 	}
@@ -15,19 +15,34 @@ var data = [
 
 $(document).ready(function(){
 	renderComments();
+	var date = new Date();
+	$(".add-comment .date").text( moment().format('MM/D/YYYY'));
 
 	$(".save-button").on('click', function(e){
 		e.preventDefault();
 		var message = $(".add-comment .message textarea").val();
-		var comment = {
-			"id": data[data.length-1].id + 1,
-			"author": $(".add-comment .username p").val(),
-			"message": $(".add-comment .message textarea").val(),
-			"date": $(".add-comment .username date").val()
+		if(message){
+			var comment = {
+				"id": data[data.length-1].id + 1,
+				"author": $(".add-comment .username").text(),
+				"message": $(".add-comment .message textarea").val(),
+				"date": $(".add-comment .date").text()
+			}
+			data.push(comment);
+			renderComments();
+			$(".add-comment .message textarea").val('');
+		} else {
+			alert("Please enter a message");
 		}
-		data.push(comment);
-		renderComments();
-	})
+	});
+
+	$(".cancel-button").on("click", function(e) {
+		e.preventDefault();
+		$(".add-comment .message textarea").val('');
+	});
+
+
+
 });
 
 function renderComments() {
